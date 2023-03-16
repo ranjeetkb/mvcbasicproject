89e35eab053a350b0b9e2bc4aa1c1c8c8b147fa7
ranjeetkaur <ranjeetkaur@BLR-REG-338332.in.niit.com>
2023-03-16 16:39:28 +0530

    Project Committed

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCBasicProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
